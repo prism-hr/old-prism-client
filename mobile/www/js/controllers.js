96bf969905c45b2f22944e288e991c9deddf8d22
juan <juan@localhost>
2016-08-10 21:38:14 +0200

    modals start point

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -56,11 +56,18 @@ function ($scope, $stateParams) {
 
 }])
 
+.controller('universityAndDepartmentCtrl', function($scope, $ionicModal) {
 
-.controller('universityAndDepartmentCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+  $scope.university = [];
 
+  $ionicModal.fromTemplateUrl('templates/universityModal.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
-}]);
+  $scope.selectUniversity = function() {
+    $scope.modal.hide();
+  };
+
+})
