6fbe0a0203bc86fb99aa4cc8e9e7e3abf63c74d2
juan <juan@localhost>
2016-08-18 11:16:32 +0200

    futher fixes for dialogs

--- web/src/app/general/header.js
+++ web/src/app/general/header.js
@@ -26,14 +26,8 @@ module.exports = {
             $state.go('register');
         };
         function DialogController($scope, $mdDialog) {
-            $scope.hide = function() {
-                $mdDialog.hide();
-            };
             $scope.cancel = function() {
                 $mdDialog.cancel();
-            };
-            $scope.answer = function(answer) {
-                $mdDialog.hide(answer);
             };
         }
     }
