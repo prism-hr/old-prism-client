49288627db6bb2d5ac4ea0474d1a4dcdb535bc0e
juan <juan@localhost>
2016-08-22 00:08:53 +0200

    employer initial and step1 start

--- web/src/app/unauthenticated/register/employer/employer-step1.js
+++ web/src/app/unauthenticated/register/employer/employer-step1.js
@@ -1,8 +1,8 @@
 module.exports = {
     templateUrl: 'app/unauthenticated/register/employer/employer-step1.html',
     controller: function ($scope) {
-        $scope.cannotFindCompany = function () {
-            $scope.notFoundCompany = true;
+        $scope.cannotFindCompany = function (bolean) {
+            $scope.notFoundCompany = bolean;
         };
     }
 };
--- web/src/app/unauthenticated/register/employer/employer.js
+++ web/src/app/unauthenticated/register/employer/employer.js
@@ -1,8 +1,6 @@
 module.exports = {
     templateUrl: 'app/unauthenticated/register/employer/employer.html',
     controller: function ($scope, $timeout, $q, $log) {
-        $scope.cannotFindCompany = function () {
-            $scope.notFoundCompany = true;
-        };
+
     }
 };
