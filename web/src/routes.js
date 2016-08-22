541426591c94aa036c5f04b03f466a9492d8352c
juan <juan@localhost>
2016-08-22 17:09:38 +0200

    merge and new studen and univeristy pages

--- web/src/routes.js
+++ web/src/routes.js
@@ -32,6 +32,16 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
             template: '<employer-step1 activity="activity" action="{{action}}" access-code="{{accessCode}}"></employer-step1>',
             data: {hideSidebar: true},
         })
+        .state('university', {
+            url: '/university',
+            template: '<university></university>',
+            data: {hideSidebar: true},
+        })
+        .state('student', {
+            url: '/student',
+            template: '<student></student>',
+            data: {hideSidebar: true},
+        })
         .state('welcome', {
             url: '/',
             template: '<welcome></welcome>',
