1eb9b9ae6821f791e4b9b58e18f073bbe846f2f0
jfi <jfi@localhost>
2016-08-23 13:45:59 +0200

    Improved step 1

--- web/src/app/create/employer/employer-step1.js
+++ web/src/app/create/employer/employer-step1.js
@@ -2,8 +2,24 @@ module.exports = {
     templateUrl: 'app/create/employer/employer-step1.html',
     controller: function () {
         var self = this;
-        this.cannotFindCompany = function (bolean) {
-            self.notFoundCompany = bolean;
+
+        this.cannotFindOrganization = function () {
+            self.organization = {};
+        };
+
+        this.lookupOrganization = function () {
+            self.organization = null;
+        };
+
+        this.organizationSelected = function (organization) {
+            self.selectedOrganization = organization;
+        };
+
+        this.confirmSelectedInstitution = function (confirm) {
+            if (confirm) {
+                self.organization = self.selectedOrganization;
+            }
+            self.selectedOrganization = null;
         };
     }
 };
