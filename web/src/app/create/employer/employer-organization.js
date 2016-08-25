1e220673bada3ea72ade11e6b71cdf41a7a91493
jfi <jfi@localhost>
2016-08-25 08:56:38 +0200

    Refactored advertise forms

--- web/src/app/create/employer/employer-organization.js
+++ web/src/app/create/employer/employer-organization.js
@@ -1,5 +1,15 @@
 module.exports = {
     templateUrl: 'app/create/employer/employer-organization.html',
     controller: function () {
+
+        this.step = 0;
+
+        this.submit = function (form) {
+            if (!form.$valid) {
+                return;
+            }
+            this.step++;
+        };
+
     }
 };
--- web/src/app/create/university/university-organization.js
+++ web/src/app/create/university/university-organization.js
@@ -1,5 +1,15 @@
 module.exports = {
     templateUrl: 'app/create/university/university-organization.html',
     controller: function () {
+
+        this.step = 0;
+
+        this.submit = function (form) {
+            if (!form.$valid) {
+                return;
+            }
+            this.step++;
+        };
+
     }
 };
