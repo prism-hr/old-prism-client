2d6e5246815ffe64117935e8398abbd58e6646e7
juan <juan@localhost>
2016-09-13 10:56:27 +0200

    updated employer fields

--- web/src/app/create/organization/organization-address.js
+++ web/src/app/create/organization/organization-address.js
@@ -6,6 +6,14 @@ module.exports = {
         form: '<'
     },
     /** @ngInject */
-    controller: function () {
+    controller: function ($scope) {
+        $scope.sizes = [
+            '1 to 9',
+            '10 to 49',
+            '50 to 99',
+            '100 to 499',
+            '500 to 999',
+            '1000+'
+        ];
     }
 };
