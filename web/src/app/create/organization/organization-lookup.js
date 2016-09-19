a7c687f724cf4a4ca3d5af7b1fca065ac6ae1861
jfi <jfi@localhost>
2016-09-19 13:26:23 +0200

    Lookup refactored

--- web/src/app/create/organization/organization-lookup.js
+++ web/src/app/create/organization/organization-lookup.js
@@ -6,8 +6,63 @@ module.exports = {
         form: '<'
     },
     /** @ngInject */
-    controller: function ($timeout, Restangular) {
+    controller: function ($q, $timeout, $state, Restangular) {
         var self = this;
+
+        this.setView = function (view) {
+            this.view = view;
+        };
+
+        this.setView(this.organization.name ? 'details' : 'lookup');
+
+        this.getOrganizations = function (searchText) {
+            return Restangular.all('organizations').getList({searchTerm: searchText})
+                .then(function (organizations) {
+                    var nameTaken = _.find(organizations, {name: searchText});
+                    if (searchText.length >= 2 && !nameTaken) {
+                        organizations.unshift({name: searchText});
+                    }
+                    return organizations;
+                });
+        };
+
+        this.organizationSelected = function () {
+            self.implementationSearchText = null;
+            self.selectedOrganizationImplementation = null;
+        };
+
+        this.getOrganizationImplementations = function (searchText) {
+            var searchPromise;
+            if (self.selectedOrganization.id) {
+                searchPromise = Restangular.one('organizations', self.selectedOrganization.id).all('organizationImplementations').getList({searchTerm: searchText})
+                    .then(function (implementations) {
+                        return implementations.plain();
+                    });
+            } else {
+                searchPromise = $q.when([]);
+            }
+
+            return searchPromise.then(function (implementations) {
+                var nameTaken = _.find(implementations, {name: searchText});
+                if (searchText.length >= 2 && !nameTaken) {
+                    implementations.unshift({name: searchText});
+                }
+                return implementations;
+            });
+        };
+
+        this.organizationImplementationSelected = function (implementation) {
+            self.organization.organization = self.selectedOrganization;
+            self.organization.name = self.selectedOrganizationImplementation.name;
+            self.setView(implementation.id ? 'request' : 'details');
+        };
+
+        this.gotoLookup = function () {
+            self.selectedOrganization = null;
+            self.selectedOrganizationImplementation = null;
+            this.setView('lookup');
+        };
+
         this.division = false;
         this.addDivision = function (bolean) {
             this.division = bolean;
@@ -16,41 +71,11 @@ module.exports = {
             }
         };
 
-        this.lookupOrganization = function () {
-            self.organization.organization = null;
-            self.foundOrganization = null;
-        };
-
-        this.getOrganizations = function (searchText) {
-            return Restangular.all('organizations').getList({searchTerm: searchText})
-                .then(function (organizations) {
-                    var list = angular.copy(organizations.plain());
-                    list.unshift({name: searchText});
-                    return list;
-                });
-        };
-
-        this.organizationSelected = function (organization) {
-            if (!organization) {
-                return;
+        this.requestAccess = function (request) {
+            if (request) {
+                // request access
             }
-            if (organization.id) {
-                self.form.foundOrganization.$setValidity('confirmed', false);
-                self.selectedOrganization = organization;
-            } else {
-                $timeout(function () { // problem with removing mask: https://github.com/angular/material/issues/9318
-                    self.organization.organization = organization;
-                });
-            }
-        };
-
-        this.confirmSelectedOrganization = function (confirm) {
-            if (confirm) {
-                self.organization.organization = self.selectedOrganization;
-            }
-            self.selectedOrganization = null;
-            self.foundOrganization = null;
-            self.form.foundOrganization.$setValidity('confirmed', true);
+            $state.go('activities');
         };
     }
 };
