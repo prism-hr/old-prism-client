a1f2eafe86e7cea5c4157723faffd64704284e62
juan <juan@localhost>
2016-10-03 15:01:50 +0200

    new colours logo edit and few small details

--- web/src/app/configuration/material.config.js
+++ web/src/app/configuration/material.config.js
@@ -20,41 +20,41 @@ export const materialConfig = function ($mdThemingProvider) {
     });
 
     $mdThemingProvider.definePalette('prismgreen', {
-        50: '#ffffff',
-        100: '#c5efe6',
-        200: '#99e3d3',
-        300: '#61d4ba',
-        400: '#48cdb0',
-        500: '#35c2a3',
-        600: '#2eaa8f',
-        700: '#28927b',
-        800: '#217a66',
-        900: '#1b6252',
-        A100: '#ffffff',
-        A200: '#c5efe6',
-        A400: '#48cdb0',
-        A700: '#28927b',
+        50: '#f0f7ee',
+        100: '#c1dfb9',
+        200: '#9fce93',
+        300: '#74b762',
+        400: '#62ac4f',
+        500: '#569745',
+        600: '#4a823b',
+        700: '#3e6d32',
+        800: '#325828',
+        900: '#26431f',
+        A100: '#f0f7ee',
+        A200: '#c1dfb9',
+        A400: '#62ac4f',
+        A700: '#3e6d32',
         contrastDefaultColor: 'light',
         contrastDarkColors: '50 100 200 300 A100 A200'
     });
 
     $mdThemingProvider.definePalette('prismred', {
         50: '#ffffff',
-        100: '#fef3e9',
-        200: '#fcd5b3',
-        300: '#f9af6f',
-        400: '#f79e51',
-        500: '#f68e34',
-        600: '#f57e17',
-        700: '#e36f0a',
-        800: '#c56009',
-        900: '#a85207',
+        100: '#fad8cd',
+        200: '#f4b09b',
+        300: '#ed7d5a',
+        400: '#ea683f',
+        500: '#e75223',
+        600: '#d44417',
+        700: '#b93c14',
+        800: '#9d3311',
+        900: '#812a0e',
         A100: '#ffffff',
-        A200: '#fef3e9',
-        A400: '#f79e51',
-        A700: '#e36f0a',
+        A200: '#fad8cd',
+        A400: '#ea683f',
+        A700: '#b93c14',
         contrastDefaultColor: 'light',
-        contrastDarkColors: '50 100 200 300 400 A100 A200 A400'
+        contrastDarkColors: '50 100 200 300 400 A100 A200'
     });
 
     $mdThemingProvider.theme('prism')
