b224262dcd67927d6d39266f852d82ce6d2aaba8
jfi <jfi@localhost>
2016-10-03 12:25:49 +0200

    Missing directives

--- web/src/app/create/advert/advert-audience.js
+++ web/src/app/create/advert/advert-audience.js
@@ -5,6 +5,22 @@ class AdvertAudienceController {
 
     $onInit() {
         this.advert.tags = this.advert.tags || [];
+
+        this.studentsTypeAll = true;
+        this.studentsUndergraduateAll = true;
+        this.studentsMastersAll = true;
+        this.studentsResearchAll = true;
+        this.studiesTypes = [
+            {category: 'Undergraduate Students', name: 'New'},
+            {category: 'Undergraduate Students', name: 'Mid Study'},
+            {category: 'Undergraduate Students', name: 'Graduating'},
+            {category: 'Masters Students', name: 'New'},
+            {category: 'Masters Students', name: 'Mid Study'},
+            {category: 'Masters Students', name: 'Graduating'},
+            {category: 'Research Students', name: 'New'},
+            {category: 'Research Students', name: 'Mid Study'},
+            {category: 'Research Students', name: 'Graduating'}
+        ];
     }
 
     lookupTags(text) {
