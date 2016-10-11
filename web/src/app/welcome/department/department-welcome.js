cf6de8fdbce92b43b1eedfd51351d6964601a275
jfi <jfi@localhost>
2016-10-11 14:44:23 +0200

    Added new welcome mechanism to department and student

--- web/src/app/welcome/department/department-welcome.js
+++ web/src/app/welcome/department/department-welcome.js
@@ -1,4 +1,14 @@
 class DepartmentWelcomeController {
+    /** @ngInject */
+    constructor(welcomeService) {
+        this.welcomeService = welcomeService;
+    }
+
+    $onInit() {
+        this.statuses = this.welcomeService.getWelcomeStatuses('department');
+        this.studentsEnabled = _.get(this.statuses.DEPARTMENT, 'resource.statusComplete.completed');
+        this.employersEnabled = _.get(this.statuses.STUDENTS, 'resource.statusComplete.completed');
+    }
 }
 
 export const DepartmentWelcome = {
