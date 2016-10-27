4c3b47d82d4125db67040aa3be6e2089e62c0b76
juan <juan@localhost>
2016-10-27 20:23:27 +0200

    logo and background controls in preview

--- web/src/app/create/organization/organization-preview-box.js
+++ web/src/app/create/organization/organization-preview-box.js
@@ -1,4 +1,16 @@
 class OrganizationPreviewBoxController {
+    constructor($mdDialog) {
+        this.$mdDialog = $mdDialog;
+    }
+    showBackgroundDialog(ev) {
+        this.$mdDialog.show({
+            template: '<background-dialog></background-dialog>',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: true
+        });
+    }
 }
 
 export const OrganizationPreviewBox = {
