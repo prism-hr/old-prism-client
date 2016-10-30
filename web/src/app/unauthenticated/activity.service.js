112b5837e61d4888caa1b6a2f67e8dc2dd07581b
jfi <jfi@localhost>
2016-10-30 20:59:24 +0100

    Listing resources in sidebar, improved authentication messages

--- web/src/app/unauthenticated/activity.service.js
+++ web/src/app/unauthenticated/activity.service.js
@@ -2,6 +2,7 @@
 export class ActivityService {
     constructor(Restangular) {
         this.Restangular = Restangular;
+        this._activitiesSubject = new Rx.Subject();
     }
 
     getActivity(accessCode) {
@@ -10,11 +11,21 @@ export class ActivityService {
 
     loadOrganizations() {
         return this.Restangular.all('organizationImplementations').getList({context: 'PROMOTER'})
-            .then(organizations => organizations.plain());
+            .then(organizations => {
+                this.organizations = organizations.plain();
+                this._activitiesSubject.onNext({organizations: this.organizations});
+            });
     }
 
     loadPromotions() {
         return this.Restangular.all('promotions').getList({category: 'EMPLOYMENT'})
-            .then(promotions => promotions.plain());
+            .then(promotions => {
+                this.promotions = promotions.plain();
+                this._activitiesSubject.onNext({promotions: this.promotions});
+            });
+    }
+
+    subscribeToActivities(observer) {
+        return this._activitiesSubject.subscribe(observer);
     }
 }
--- web/src/app/unauthenticated/authenticate/authenticate.js
+++ web/src/app/unauthenticated/authenticate/authenticate.js
@@ -11,6 +11,7 @@ class AuthenticateController {
 
     setView(view) {
         this.view = view;
+        this.responseError = null;
         const user = _.get(this.activity, 'activity.userRole.user') || _.get(this.activity, 'activity.user');
         this.user = view === 'FORGOT_PASSWORD' ? {} : angular.copy(user) || {};
     }
@@ -19,39 +20,48 @@ class AuthenticateController {
         if (!form.$valid) {
             return;
         }
+        this.responseError = null;
         this.loading = true;
         this.authService.login(_.pick(this.user, ['username', 'password']))
             .then(this.$mdDialog.hide)
-            .finally(this.resetLoading);
+            .catch(r => this.handleError(r))
+            .finally(() => this.resetLoading());
     }
 
     register(form) {
         if (!form.$valid) {
             return;
         }
+        this.responseError = null;
         this.loading = true;
         this.authService.register(this.user)
             .then(this.$mdDialog.hide)
-            .finally(this.resetLoading);
+            .finally(() => this.resetLoading());
     }
 
     oauth(provider) {
+        this.responseError = null;
         this.loading = true;
         this.authService.authenticate(provider)
             .then(this.$mdDialog.hide)
-            .finally(this.resetLoading);
+            .finally(() => this.resetLoading());
     }
 
     resetPassword(form) {
         if (!form.$valid) {
             return;
         }
+        this.responseError = null;
         this.loading = true;
         this.authService.resetPassword(this.user)
             .then(() => {
                 this.passwordReset = true;
             })
-            .finally(this.resetLoading);
+            .finally(() => this.resetLoading());
+    }
+
+    handleError(response) {
+        this.responseError = response.data.responseState;
     }
 
     resetLoading() {
