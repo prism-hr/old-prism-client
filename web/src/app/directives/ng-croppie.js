8608d602bc730655ce1c461ed54d978b5e7d7e19
jfi <jfi@localhost>
2016-11-07 15:22:23 +0100

    Improved background cropping, added buttons

--- web/src/app/directives/ng-croppie.js
+++ web/src/app/directives/ng-croppie.js
@@ -14,19 +14,26 @@ export class NgCroppie {
     link(scope, element, attrs, ngModel) {
         const self = this;
         const containerWidth = angular.element(document.body.querySelectorAll('.background-box'))[0].clientWidth;
-        const croppie = new Croppie(element[0], {
+        const options = {
             viewport: {width: containerWidth, height: 320},
             boundary: {width: containerWidth, height: 320}
-        });
+        };
+
+        options.update = function (result, b, c) {
+            const cropPoints = result.points;
+            const x = cropPoints[0];
+            const y = cropPoints[1];
+            const width = cropPoints[2] - x;
+            const height = cropPoints[3] - y;
+            const crop = 'crop';
+            ngModel.$setViewValue(self.cloudinary.url(scope.srcPublicId, {x, y, width, height, crop}));
+        };
+        const croppie = new Croppie(element[0], options);
 
         scope.$watch('srcPublicId', function (src) {
             if (src) {
                 croppie.bind(self.cloudinary.url(src));
             }
         });
-
-        function updateModel() {
-            const cropPoints = croppie.get();
-        }
     }
 }
