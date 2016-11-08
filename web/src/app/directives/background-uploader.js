dc2e58af5e6b3553702d89ae7c57e43e3621d241
jfi <jfi@localhost>
2016-11-08 15:57:37 +0100

    Cropper refactored

--- web/src/app/directives/background-uploader.js
+++ web/src/app/directives/background-uploader.js
@@ -1,10 +1,9 @@
 export class BackgroundUploader {
     /** @ngInject */
-    constructor(UploadDataUrl, cloudinary, environment) {
+    constructor(cloudinary, environment) {
         this.template = require('./background-uploader.html');
         this.require = 'ngModel';
         this.restrict = 'E';
-        this.UploadDataUrl = UploadDataUrl;
         this.cloudinary = cloudinary;
         this.environment = environment;
     }
@@ -13,38 +12,42 @@ export class BackgroundUploader {
         const self = this;
         scope.data = {};
         const CLOUDINARY_REGEX = /^.+\.cloudinary\.com\/(?:[^\/]+\/)(?:(image|video)\/)?(?:(upload|fetch)\/)?(?:(?:[^_/]+_[^,/]+,?)*\/)?(?:v(\d+|\w{1,2})\/)?([^\.^\s]+)(?:\.(.+))?$/;
+        scope.publicId = 'local/jst7pa4karrnpzw0hd89';
 
         scope.fileChanged = function (file) {
-            scope.mimeType = file.type;
-            self.UploadDataUrl.dataUrl(file)
-                .then(function (url) {
-                    scope.srcUrl = url;
-                });
-        };
-
-        ngModel.$render = function () {
-            scope.publicId = ngModel.$modelValue && getPublicId(ngModel.$modelValue);
-        };
-
-        scope.cancel = function () {
-            scope.srcUrl = null;
-            scope.data.croppedUrl = null;
-        };
-
-        scope.confirm = function () {
-            self.cloudinary.upload(scope.data.croppedImage, {folder: self.environment.cloudinaryFolder})
+            if (!file) {
+                return;
+            }
+            self.cloudinary.upload(file, {folder: self.environment.cloudinaryFolder})
                 .then(response => {
                     scope.progressPercentage = null;
-                    scope.srcUrl = null;
-                    scope.data.croppedUrl = null;
-                    ngModel.$setViewValue(response.data.url);
-                    scope.publicId = getPublicId(response.data.url);
+                    scope.publicIdToCrop = getPublicId(response.data.url);
                 }, response => {
                     scope.progressPercentage = null;
                     scope.error = response.status;
                 }, event => {
                     scope.progressPercentage = Math.round(100.0 * event.loaded / event.total);
                 });
+        };
+
+        ngModel.$render = function () {
+            scope.imageUrl = ngModel.$modelValue;
+        };
+
+        scope.edit = function () {
+            scope.publicIdToCrop = ngModel.$modelValue && getPublicId(ngModel.$modelValue);
+        };
+
+        scope.cancel = function () {
+            scope.publicIdToCrop = null;
+            scope.data.croppedUrl = null;
+        };
+
+        scope.confirm = function () {
+            scope.imageUrl = scope.data.croppedUrl;
+            ngModel.$setViewValue(scope.imageUrl);
+            scope.publicIdToCrop = null;
+            scope.data.croppedUrl = null;
         };
 
         function getPublicId(url) {
--- web/src/app/directives/ng-croppie.js
+++ web/src/app/directives/ng-croppie.js
@@ -6,33 +6,33 @@ export class NgCroppie {
         this.require = 'ngModel';
         this.restrict = 'E';
         this.scope = {
-            srcUrl: '@',
-            mimeType: '@'
+            publicId: '@'
         };
         this.cloudinary = cloudinary;
     }
 
     link(scope, element, attrs, ngModel) {
         const self = this;
-        let croppie = null;
         const containerWidth = angular.element(document.body.querySelectorAll('.background-box'))[0].clientWidth;
         const options = {
             viewport: {width: containerWidth, height: 320},
             boundary: {width: containerWidth, height: 320}
         };
 
-        options.update = function () {
-            const format = scope.mimeType.replace('image/', '');
-            croppie.result({type: 'blob', format: format})
-                .then(result => {
-                    ngModel.$setViewValue(result);
-                });
+        options.update = function (result) {
+            const cropPoints = result.points;
+            const x = cropPoints[0];
+            const y = cropPoints[1];
+            const width = cropPoints[2] - x;
+            const height = cropPoints[3] - y;
+            const crop = 'crop';
+            ngModel.$setViewValue(self.cloudinary.url(scope.publicId, {x, y, width, height, crop}));
         };
-        croppie = new Croppie(element[0], options);
+        const croppie = new Croppie(element[0], options);
 
-        scope.$watch('srcUrl', function (src) {
+        scope.$watch('publicId', src => {
             if (src) {
-                croppie.bind(src);
+                croppie.bind(self.cloudinary.url(src));
             }
         });
     }
