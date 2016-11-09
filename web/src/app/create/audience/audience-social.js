23902751e38eabc98e6b673ed8574e80ff158f95
juan <juan@localhost>
2016-11-09 18:20:14 +0100

    update Social Sharing

--- web/src/app/create/audience/audience-social.js
+++ web/src/app/create/audience/audience-social.js
@@ -1,4 +1,14 @@
 class AudienceSocialController {
+    $onInit() {
+        this.selectedIndex = 0;
+    }
+    changeTab(number, $event) {
+        const selectedTab = angular.element($event.currentTarget);
+        const lastTab = angular.element($event.currentTarget.parentElement.querySelector('.active'));
+        lastTab.removeClass('active');
+        selectedTab.addClass('active');
+        this.selectedIndex = number;
+    }
 }
 
 export const AudienceSocial = {
--- web/src/app/create/audience/audience-summary.js
+++ web/src/app/create/audience/audience-summary.js
@@ -2,17 +2,9 @@ class AudienceSummaryController {
     constructor(definitions) {
         this.definitions = definitions;
     }
-    $onInit() {
-        this.patternValues = this.definitions.studentPattern;
-        this.selectedIndex = 0;
-    }
 
-    changeTab(number, $event) {
-        const selectedTab = angular.element($event.currentTarget);
-        const lastTab = angular.element($event.currentTarget.parentElement.querySelector('.active'));
-        lastTab.removeClass('active');
-        selectedTab.addClass('active');
-        this.selectedIndex = number;
+    $onInit() {
+        this.promotionAllowAccess = this.promotionAllowAccess || true;
     }
 }
 
