0ee553e774d10556c2f8ca00297ff292e16ab55d
juan <juan@localhost>
2016-11-23 14:02:36 +0100

    student header end

--- web/src/app/create/student/student-header.js
+++ web/src/app/create/student/student-header.js
@@ -1,13 +1,17 @@
 class StudentHeaderController {
-    constructor($q, $state, Restangular, cloudinary, welcomeService) {
+    constructor($q, $state, Restangular, cloudinary, welcomeService, authService, userSessionService) {
         this.$q = $q;
         this.$state = $state;
         this.Restangular = Restangular;
         this.cloudinary = cloudinary;
         this.welcomeService = welcomeService;
+        this.authService = authService;
+        this.userSessionService = userSessionService;
     }
+
     $onInit() {
         this.showImplementationName = this.wizardType === 'student';
+        this.student.email = this.student.email || this.authService.user.username;
     }
 }
 
