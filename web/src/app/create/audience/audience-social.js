0d9c1b99a11c6d2388985c11052dc5416112a9a7
jfi <jfi@localhost>
2016-11-28 12:12:13 +0100

    Some issues with Facebook fixed

--- web/src/app/create/audience/audience-social.js
+++ web/src/app/create/audience/audience-social.js
@@ -1,7 +1,13 @@
 class AudienceSocialController {
+    constructor(environment) {
+        this.environment = environment;
+        this.facebookAppId = environment.oauth.facebook;
+    }
+
     $onInit() {
         this.selectedIndex = 0;
     }
+
     changeTab(number, $event) {
         const selectedTab = angular.element($event.currentTarget);
         const lastTab = angular.element($event.currentTarget.parentElement.querySelector('.active'));
