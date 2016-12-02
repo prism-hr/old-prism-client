ac627d40eddae433831277b77bb7306f8edb675a
juan <juan@localhost>
2016-12-02 10:47:36 +0100

    studies funcionality mockup

--- web/src/app/create/student/student-studies.js
+++ web/src/app/create/student/student-studies.js
@@ -1,4 +1,21 @@
 class StudentStudiesController {
+    /** @ngInject */
+    constructor(definitions) {
+        this.definitions = definitions;
+    }
+
+    $onInit() {
+        this.studyList = false;
+        this.studyForm = true;
+    }
+    fakeSave() {
+        this.studyList = true;
+        this.studyForm = false;
+    }
+    fakeMore() {
+        this.studyList = false;
+        this.studyForm = true;
+    }
 }
 
 export const StudentStudies = {
