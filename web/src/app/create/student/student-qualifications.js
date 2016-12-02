ca5b95621d42365ef2e6229f148391687ca92bcf
jfi <jfi@localhost>
2016-12-02 15:10:21 +0100

    Added logi nto student qualifications

--- web/src/app/create/student/student-qualifications.js
+++ web/src/app/create/student/student-qualifications.js
@@ -4,23 +4,34 @@ class StudentQualificationsController {
         this.definitions = definitions;
     }
 
-    $onInit() {
-        this.studyList = false;
-        this.studyForm = true;
+    editQualification(qualification) {
+        this.editedQualification = angular.copy(qualification);
     }
-    fakeSave() {
-        this.studyList = true;
-        this.studyForm = false;
+
+    addQualification() {
+        this.editedQualification = {};
     }
-    fakeMore() {
-        this.studyList = false;
-        this.studyForm = true;
+
+    saveQualification() {
+        const qualifications = this.student.userQualifications;
+        const idx = qualifications.findIndex(q => q.accessCode === this.editedQualification.accessCode);
+        if (idx > -1) {
+            qualifications.splice(idx, 1, this.editedQualification);
+        } else {
+            qualifications.push(this.editedQualification);
+        }
+        this.editedQualification = null;
+    }
+
+    cancelQualification() {
+        this.editedQualification = null;
     }
 }
 
 export const StudentQualifications = {
     template: require('./student-qualifications.html'),
     bindings: {
+        wizardType: '@',
         student: '=',
         form: '<'
     },
