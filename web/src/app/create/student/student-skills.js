df38fde9cad956da09ec2d112c62eb7013a77c91
juan <juan@localhost>
2016-12-02 19:53:31 +0100

    user skills

--- web/src/app/create/student/student-skills.js
+++ web/src/app/create/student/student-skills.js
@@ -1,4 +1,15 @@
 class StudentSkillsController {
+    lookupTags(text) {
+        return this.Restangular.all('tags').getList({searchTerm: text})
+            .then(tags => tags.plain());
+    }
+
+    transformTag(chip) {
+        if (angular.isObject(chip)) {
+            return {tag: _.pick(chip, ['id', 'name'])};
+        }
+        return {tag: {name: chip}};
+    }
 }
 
 export const StudentSkills = {
