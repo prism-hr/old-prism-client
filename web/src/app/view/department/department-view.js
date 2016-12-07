1a14ea2da09446b14bbedceab717f4241e20e6c3
juan <juan@localhost>
2016-12-07 21:30:09 +0100

    department view start

--- web/src/app/view/department/department-view.js
+++ web/src/app/view/department/department-view.js
@@ -3,6 +3,13 @@ class DepartmentViewController {
         this.selectedIndex = 0;
         this.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
     }
+    changeTab(number, $event) {
+        const selectedTab = angular.element($event.currentTarget);
+        const lastTab = angular.element($event.currentTarget.parentElement.querySelector('.active'));
+        lastTab.removeClass('active');
+        selectedTab.addClass('active');
+        this.selectedIndex = number;
+    }
 }
 
 export const DepartmentView = {
