080c4ba9d36148368619b0f7cc7d945ef94631a6
jfi <jfi@localhost>
2016-12-09 14:54:52 +0100

    View and count added

--- web/src/app/create/wizard-header.js
+++ web/src/app/create/wizard-header.js
@@ -1,7 +1,13 @@
 class WizardHeaderController {
+    /** @ngInject */
+    constructor($state) {
+        this.$state = $state;
+    }
+
     $onInit() {
         this.stepSubscription = this.wizard.stepSubscribe(this._onStepChange.bind(this));
         this._onStepChange(this.wizard.getCurrentStep());
+        this.viewHref = this.$state.href('view.' + this.wizard.getWizardType(), {accessCode: this.wizard.getResource().accessCode});
     }
 
     _onStepChange(currentStep) {
