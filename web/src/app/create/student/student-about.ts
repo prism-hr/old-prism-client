ac6156a7fa103586178ad9e4762e11892558fc80
jfi <jfi@localhost>
2016-12-17 12:26:37 +0100

    User wizard relocation

--- web/src/app/create/student/student-about.ts
+++ web/src/app/create/student/student-about.ts
@@ -1,60 +1,16 @@
 import {AuthService} from '../../unauthenticated/auth.service';
-import {countries} from '../../../countries';
 import UserRepresentation = bigfoot.UserRepresentation;
 import LanguageRelationRepresentation = bigfoot.LanguageRelationRepresentation;
 
 class StudentAboutController {
     private student: UserRepresentation;
-    private locationType: LocationType;
-    private countries = countries;
-
-    /** @ngInject */
-    constructor(private definitions: any, private authService: AuthService) {
-    }
-
-    $onInit() {
-        if (this.student.languages.length < 1) {
-            this.student.languages.push(<LanguageRelationRepresentation>{language: undefined});
-        }
-        this.student.proximity = this.student.proximity || 'TO_200';
-        if (this.student.anywhere) {
-            this.locationType = 'ANYWHERE';
-        } else {
-            this.locationType = this.student.country ? 'COUNTRY' : 'CITY';
-        }
-    }
-
-    locationTypeChanged(locationType: LocationType) {
-        this.student.anywhere = locationType === 'ANYWHERE';
-        this.student.country = !this.student.anywhere && locationType === 'COUNTRY';
-        this.student.locations = [];
-        this.student.proximity = null;
-    };
-
-    lookupCountries(searchText: string) {
-        searchText = searchText.toLowerCase();
-        return this.countries.filter(c => c.name.toLowerCase().startsWith(searchText));
-    }
-
-    addLanguage(language: LanguageRelationRepresentation) {
-        const idx = this.student.languages.indexOf(language);
-        this.student.languages.splice(idx + 1, 0, <LanguageRelationRepresentation>{language: undefined});
-    }
-
-    removeLanguage(language: LanguageRelationRepresentation) {
-        const idx = this.student.languages.indexOf(language);
-        this.student.languages.splice(idx, 1);
-    }
 }
 
 export const StudentAbout = {
     template: require('./student-about.html'),
     bindings: {
-        wizardType: '@',
         student: '=',
         form: '<'
     },
     controller: StudentAboutController
 };
-
-type LocationType = 'ANYWHERE' | 'COUNTRY' | 'CITY';
--- web/src/app/create/student/student-skills.ts
+++ web/src/app/create/student/student-skills.ts
@@ -1,14 +1,40 @@
 import * as _ from 'lodash';
 import * as angular from 'angular';
+import {countries} from '../../../countries';
 import UserRepresentation = bigfoot.UserRepresentation;
+import LanguageRelationRepresentation = bigfoot.LanguageRelationRepresentation;
 
 class StudentSkillsController {
     private separatorKeys: Array<string>;
     private student: UserRepresentation;
+    private locationType: LocationType;
+    private countries = countries;
 
     /** @ngInject */
-    constructor(private Restangular: Restangular.IService, private $mdConstant: any) {
+    constructor(private Restangular: Restangular.IService, private $mdConstant: any, private definitions: any) {
         this.separatorKeys = [$mdConstant.KEY_CODE.ENTER, $mdConstant.KEY_CODE.COMMA];
+    }
+
+    $onInit() {
+        if (this.student.languages.length < 1) {
+            this.student.languages.push(<LanguageRelationRepresentation>{language: undefined});
+        }
+        this.student.proximity = this.student.proximity || 'TO_200';
+        if (this.student.anywhere) {
+            this.locationType = 'ANYWHERE';
+        } else {
+            this.locationType = this.student.country ? 'COUNTRY' : 'CITY';
+        }
+    }
+
+    addLanguage(language: LanguageRelationRepresentation) {
+        const idx = this.student.languages.indexOf(language);
+        this.student.languages.splice(idx + 1, 0, <LanguageRelationRepresentation>{language: undefined});
+    }
+
+    removeLanguage(language: LanguageRelationRepresentation) {
+        const idx = this.student.languages.indexOf(language);
+        this.student.languages.splice(idx, 1);
     }
 
     lookupTags(text: string) {
@@ -36,13 +62,28 @@ class StudentSkillsController {
         }
         return {interest: {name: chip}};
     }
+
+    locationTypeChanged(locationType: LocationType) {
+        this.student.anywhere = locationType === 'ANYWHERE';
+        this.student.country = !this.student.anywhere && locationType === 'COUNTRY';
+        this.student.locations = [];
+        this.student.proximity = null;
+    };
+
+    lookupCountries(searchText: string) {
+        searchText = searchText.toLowerCase();
+        return this.countries.filter(c => c.name.toLowerCase().startsWith(searchText));
+    }
 }
 
 export const StudentSkills = {
     template: require('./student-skills.html'),
     bindings: {
         student: '=',
-        form: '<'
+        form: '<',
+        wizardType: '@'
     },
     controller: StudentSkillsController
 };
+
+type LocationType = 'ANYWHERE' | 'COUNTRY' | 'CITY';
